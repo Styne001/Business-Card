84c411f1d38f2b5c98b493e2efa4610bae798534
Celestine <47569127+Styne001@users.noreply.github.com>
2022-09-18 13:41:46 +0100

    Edited Image names

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,19 +5,19 @@ export default function Footer() {
     return (
         <main className="footer">
             <a href="https://twitter.com/Styne001">
-                <img src="../src/assets/Twitter Icon.png"/>
+                <img src="../src/assets/Twitter_Icon.png"/>
             </a>
             <a href="">
-                <img src="../src/assets/Facebook Icon.png"/>
+                <img src="../src/assets/Facebook_Icon.png"/>
             </a>
             <a href="">
-                <img src="../src/assets/Instagram Icon.png"/>
+                <img src="../src/assets/Instagram_Icon.png"/>
             </a>
             <a href="https://www.linkedin.com/in/celestine-okonkwo-954a11174/">
-                <img src="../src/assets/Linkedin Icon.png"/>
+                <img src="../src/assets/Linkedin_Icon.png"/>
             </a>
             <a href="https://github.com/Styne001">
-                <img src="../src/assets/Github Icon.png"/>
+                <img src="../src/assets/Github_Icon.png"/>
             </a>
         </main>
     )
