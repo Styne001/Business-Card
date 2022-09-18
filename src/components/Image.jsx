6c5a23e2d71ed5d6255f02dfd7ad24927f95a534
Celestine <47569127+Styne001@users.noreply.github.com>
2022-09-18 13:54:11 +0100

    Updated Image parts

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Image() {
     return (
         <main className="main">
-            <img src="../src/assets/styne.jpeg" className="styne"/>
+            <img src="/src/assets/styne.jpeg" className="styne"/>
         </main>
     )
 }
